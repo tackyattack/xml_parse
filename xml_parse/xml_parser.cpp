057b21c400ece6f2bb321b469a0e74ba43e9e440
HENRY BERGIN <tackyattack@gmail.com>
2017-10-17 20:45:52 -0500

    first commit

---
+++ xml_parse/xml_parser.cpp
@@ -0,0 +1,11 @@
+//
+//  xml_parser.cpp
+//  xml_parse
+//
+//  Created by HENRY BERGIN on 10/17/17.
+//  Copyright © 2017 Henry Bergin. All rights reserved.
+//
+
+#include "xml_parser.h"
+
+
---
+++ xml_parse/xml_parser.h
@@ -0,0 +1,19 @@
+//
+//  xml_parser.h
+//  xml_parse
+//
+//  Created by HENRY BERGIN on 10/17/17.
+//  Copyright © 2017 Henry Bergin. All rights reserved.
+//
+
+#ifndef xml_parser_h
+#define xml_parser_h
+
+class xml_parser
+{
+public:
+private:
+    
+};
+
+#endif /* xml_parser_h */
