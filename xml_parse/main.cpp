48e8e0b6facc36a32cf8338bbb00da6b924ff503
HENRY BERGIN <tackyattack@gmail.com>
2017-10-17 20:41:22 -0500

    Initial Commit

---
+++ xml_parse/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  xml_parse
+//
+//  Created by HENRY BERGIN on 10/17/17.
+//  Copyright © 2017 Henry Bergin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
